1a993a11096559f52ec07914e6955b9df29a6dce
soyeonvv <sovvworld@gmail.com>
2024-06-05 11:21:12 +0900

    [design] pie chart css 수정

--- src/pages/PieChart.js
+++ src/pages/PieChart.js
@@ -2,29 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
-
-const styles = {
-  contentWrap: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100vh',
-    boxSizing: 'border-box',
-  },
-  chartTitle: {
-    textAlign: 'center',
-    marginBottom: '20px',
-    fontSize: '24px',
-  },
-  contentInner: {
-    width: '80%',
-    maxWidth: '600px',
-  },
-};
+import '../styles/chart.css';
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Page = () => {
   const [datalist, setDatalist] = useState([]);
@@ -67,12 +46,14 @@ const Page = () => {
     ],
   };
   return (
-    <div className='contentWrap' style={styles.contentWrap}>
-      <div style={styles.chartTitle}>
+    <div className='contentWrap'>
+      <div className='chartTitle'>
         상품별 가입자 수 비율
       </div>
-      <div className='contentInner' style={styles.contentInner}>
-        <Pie data={data} />
+      <div className='contentInner'>
+        <div className='contentPieChart'>
+          <Pie data={data} />
+        </div>
       </div>
     </div>
   );
