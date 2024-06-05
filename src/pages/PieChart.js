026b6eda79d90b0539308a613be54133207b7fa8
Wonhee <totohwh3601@naver.com>
2024-06-05 10:26:54 +0900

    [refactor] PieChart url 경로 수정 로컬 -> 서버

--- src/pages/PieChart.js
+++ src/pages/PieChart.js
@@ -30,7 +30,7 @@ const Page = () => {
   const [datalist, setDatalist] = useState([]);
 
   useEffect(() => {
-    const url = 'http://127.0.0.1:8080/admin/piechart';
+    const url = 'http://13.125.8.139:8080/admin/piechart';
     axios
       .get(url)
       .then((res) => {
